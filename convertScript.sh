85a01976c0f4a8285844c9f42275cbf5f17a4927
wbt11a <wbt11a@users.noreply.github.com>
2020-08-12 20:18:16 +0300

    Update convertScript.sh

--- convertScript.sh
+++ convertScript.sh
@@ -5,7 +5,7 @@ outputDir="./h265vids/"
 CRF="22"
 FILE_TYPES=("avi" "mp4" "mkv")
 PROCESS_PATH="/plex/plex/adult/tv/" #MAKE SURE YOU HAVE / AT END
-EXCLUDE_DIRS=("./Greatest Events of WWII in Colour/" "./NewGirl/Season 3/")
+EXCLUDE_DIRS=("./Greatest Events of WWII in Colour/" "./NewGirl/S03/" "./Outlander/s01/")
 cd "$PROCESS_PATH"
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
@@ -14,8 +14,8 @@ IFS=$(echo -en "\n\b")
 
 #echo "testing " "$outputDir/completed.lock"
 if [ -f "$outputDir/completed.lock" ]; then
-		echo "area already converted.  Exiting"
-		exit 0
+	echo "area already converted.  Exiting"
+	exit 0
 fi
 
 FILE_FLAGS=""
@@ -24,17 +24,17 @@ last=$((len-1))
 
 for ((i=0; i<$len;i++))
 do
-	
-		if [ "$len" -eq "1" ]; then
-			FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\""
-			continue
-		fi
 
-		if [ "$i" -eq "$last" ]; then
-			FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\""
-		else
-			FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\" -o"
-		fi
+	if [ "$len" -eq "1" ]; then
+		FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\""
+		continue
+	fi
+
+	if [ "$i" -eq "$last" ]; then
+		FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\""
+	else
+		FILE_FLAGS="${FILE_FLAGS} -name \"*.${FILE_TYPES[i]}\" -o"
+	fi
 done
 
 
@@ -44,7 +44,7 @@ IGNORE_DIR_FLAGS="-not -path \"${outputDir}*\" -prune"
 #parse exclude dirs array into find command arguments
 for i in "${EXCLUDE_DIRS[@]}"
 do
-		IGNORE_DIR_FLAGS="${IGNORE_DIR_FLAGS} -not -path \"${i}*\" -prune"
+	IGNORE_DIR_FLAGS="${IGNORE_DIR_FLAGS} -not -path \"${i}*\" -prune"
 done
 
 MY_COMMAND="/usr/bin/find . -type f ${FILE_FLAGS} ${IGNORE_DIR_FLAGS} -print0 | sort"
@@ -57,45 +57,46 @@ mapfile -d $'\0' MY_RESULTS < <(eval "$MY_COMMAND")
 #for f in `find . -type f -name "*.avi" -o -name "*.mp4" -o -name "*.mkv" -not -path ${PROCESS_PATH}${outputDir} | sort`;
 for f in "${MY_RESULTS[@]}"
 do
-  	audioformat=$(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$f")
-		videoformat=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$f")
+	audioformat=$(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$f")
+	videoformat=$(ffprobe -loglevel error -select_streams v:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$f")
 
-  	if [ "$videoformat" = "hevc" ]; then
-			echo "${f} is already hevc encoded - skipping."
-			continue
-		fi
+	if [ "$videoformat" = "hevc" ]; then
+		echo "${f} is already hevc encoded - skipping."
+		continue
+	fi
 
-		MYFILE=$(basename "$f")
-		FILESIZE=$(stat -c%s "$f")
-		echo "Base filesize: ${FILESIZE}"
-		
-		if [ "$audioformat" = "aac" ]; then
-			#/usr/bin/ffmpeg -y -i "$f" -c:v libx265 -x265-params crf="$CRF" -c:a copy "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
-			/usr/local/bin/ffpb -y -i "$f" -map_chapters 0 -map_metadata 0 -c:s copy -c:v libx265 -x265-params crf="$CRF" -c:a copy "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
-		else
-			#/usr/bin/ffmpeg -y -i "$f" -c:v libx265 -x265-params crf="$CRF" -c:a aac "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
-			/usr/local/bin/ffpb -y -i "$f" -map_chapters 0 -map_metadata 0 -c:s copy -c:v libx265 -x265-params crf="$CRF" -c:a aac "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
-		fi
+	MYFILE=$(basename "$f")
+	FILESIZE=$(stat -c%s "$f")
+	echo "Base filesize: ${FILESIZE}"
 
-		#echo "running mv ${outputDir}${MYFILE%.*}.x265.mkv" $(dirname "$f")
+	if [ "$audioformat" = "aac" ]; then
+		#/usr/bin/ffmpeg -y -i "$f" -c:v libx265 -x265-params crf="$CRF" -c:a copy "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
+		/usr/local/bin/ffpb -y -i "$f" -map_chapters 0 -map_metadata 0 -c:s copy -c:v libx265 -x265-params crf="$CRF" -c:a copy "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
+	else
+		#/usr/bin/ffmpeg -y -i "$f" -c:v libx265 -x265-params crf="$CRF" -c:a aac "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
+		/usr/local/bin/ffpb -y -i "$f" -map_chapters 0 -map_metadata 0 -c:s copy -c:v libx265 -x265-params crf="$CRF" -c:a aac "$PROCESS_PATH""$outputDir"/"${MYFILE%.*}.x265.mkv"
+	fi
 
-		myDir=$(dirname "$f")
-		NEWFILESIZE=$(stat -c%s "${outputDir}${MYFILE%.*}.x265.mkv")
-		echo "New filesize: ${NEWFILESIZE}"
-		CMD="mv \"${outputDir}${MYFILE%.*}.x265.mkv\" \"${myDir}\""
-		echo "Running ${CMD}"
-		eval "$CMD"
-		#  mv "$PROCESS_PATH""$outputDir/""${MYFILE%.*}.x265.mkv" $(dirname "$f")
-		CMD="mv \"${f}\" \"${outputDir}\""
-		echo "Running ${CMD}"
-		eval "$CMD"
+	#echo "running mv ${outputDir}${MYFILE%.*}.x265.mkv" $(dirname "$f")
+	myDir=$(dirname "$f")
+	NEWFILESIZE=$(stat -c%s "${outputDir}${MYFILE%.*}.x265.mkv")
+	echo "New filesize: ${NEWFILESIZE}"
+	CMD="mv \"${outputDir}${MYFILE%.*}.x265.mkv\" \"${myDir}\""
+	echo "Running ${CMD}"
+	eval "$CMD"
 
-		if [ "$FILESIZE" -lt "$NEWFILESIZE" ]; then
-			echo "WARNING: ${f} is smaller than its converted form." >> CONVERTFILE.WARNINGS.txt
-		fi
+	#  mv "$PROCESS_PATH""$outputDir/""${MYFILE%.*}.x265.mkv" $(dirname "$f")
+	CMD="mv \"${f}\" \"${outputDir}\""
+	echo "Running ${CMD}"
+	eval "$CMD"
 
-		#  mv "$f" "$PROCESS_PATH""$outputDir"
+	if [ "$FILESIZE" -lt "$NEWFILESIZE" ]; then
+		echo "WARNING: ${f} is smaller than its converted form." >> CONVERTFILE.WARNINGS.txt
+	fi
+
+	#  mv "$f" "$PROCESS_PATH""$outputDir"
 done
 
 touch "$outputDir/completed.lock"
 IFS=$SAVEIFS
+
